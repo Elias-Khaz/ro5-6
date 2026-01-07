67fd249c976271d92f5c72c1c816196908d41a18
Elias-Khaz <elias.khazroun5@gmail.com>
2026-01-07 16:09:03 +0100

    finished registration

--- ro5-6/src/App.jsx
+++ ro5-6/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import JokeGame from "./components/JokeGame";
 
 function App() {
   return (
-    <div>
-      <JokeGame />
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Register />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/game" element={<JokeGame />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ ro5-6/src/pages/Register.jsx
@@ -0,0 +1,60 @@
+import { usestate } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Auth.css";
+
+function Register() {
+    const [username, setUsername] = usestate("");
+    const [password, setPassword] = usestate("");
+    const [error, setError] = usestate("");
+    const navigate = useNavigate();
+
+    function handleRegister(e) {
+        e.preventDefault();
+
+        const hasSymbol = /[^a-zA-Z0-9]/.test(password);
+
+        if (password.length < 8) {
+            setError("Password must be at least 8 characters");
+            return;
+        }
+
+        if (!hasSymbol) {
+            setError("Password must contain a symbol")
+        }
+
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+
+        navigate("/login");
+    }
+
+    return (
+        <div className="auth-container">
+            <h2>Register</h2>
+
+            <form onSubmit={handleRegister}>
+                <input 
+                type="text" 
+                placeholder="Username"
+                value={username}
+                required
+                onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input 
+                type="password" 
+                placeholder="Password"
+                value={password}
+                required
+                onChange={(e) => setPassword(e.target.value)}
+                />
+
+                {error && <p className="error">{error}</p>}
+
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
